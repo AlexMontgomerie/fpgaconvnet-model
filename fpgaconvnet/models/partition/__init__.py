1499012f7196ab9cb4768b45ee47321ba632ee19
AlexMontgomerie <am9215@ic.ac.uk>
2022-05-13 15:26:35 +0100

    fixed module import for Partition

--- fpgaconvnet/models/partition/__init__.py
+++ fpgaconvnet/models/partition/__init__.py
@@ -1,3 +1,5 @@
 """
 Partitions make up a sub-graph of the CNN model network. They are comprised of layers. A single partition fits on an FPGA at a time, and partitions are changed by reconfiguring the FPGA.
 """
+
+from .Partition import Partition
