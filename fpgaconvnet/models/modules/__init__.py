3999d08e07116591f0ef5e65e746b78307d213aa
Petros Toupas <ptoupas@gmail.com>
2022-10-20 14:10:58 +0100

    add bias3d functional model

--- fpgaconvnet/models/modules/__init__.py
+++ fpgaconvnet/models/modules/__init__.py
@@ -17,12 +17,14 @@ from .Fork import Fork3D
 from .Glue import Glue
 from .Glue import Glue3D
 from .Pool import Pool
+from .Pool import Pool3D
 from .ReLU import ReLU
 from .ReLU3D import ReLU3D
 from .SlidingWindow import SlidingWindow
 from .SlidingWindow import SlidingWindow3D
 from .Squeeze import Squeeze
 from .Bias import Bias
+from .Bias import Bias3D
 from .VectorDot import VectorDot
 from .VectorDot import VectorDot3D
 from .AveragePool import AveragePool
