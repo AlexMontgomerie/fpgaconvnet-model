e78798b7dc76eec67d916f41c54b1d2a53e5c2e4
AlexMontgomerie <am9215@ic.ac.uk>
2022-11-21 23:05:25 +0000

    updated resource model coefficients

--- fpgaconvnet/scripts/load_resource_models.py
+++ fpgaconvnet/scripts/load_resource_models.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 
 from fpgaconvnet.tools.resource_regression_model import ModuleModel
 
-# CHISEL_MODULES = [ "Accum", "Fork", "Glue", "SlidingWindow",
-#     "Squeeze", "VectorDot" , "MaxPool", "AveragePool" ]
-CHISEL_MODULES = [ "AveragePool" ]
+CHISEL_MODULES = [ "Accum", "Fork", "Glue", "SlidingWindow",
+    "Squeeze", "VectorDot" , "MaxPool", "AveragePool" , "Bias" ]
+# CHISEL_MODULES = [ "AveragePool" ]
 HLS_MODULES = []
 
 # iterate over chisel modules
@@ -92,7 +92,7 @@ for module in CHISEL_MODULES:
         plt.xlabel("actual")
         plt.ylabel("predicted")
         plt.legend()
-        plt.show()
+        # plt.show()
 
 #     for rsc_type in self.rsc_types:
     #         fig, ax = plt.subplots(figsize=(10, 6))
