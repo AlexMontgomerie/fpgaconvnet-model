d06e63662ec15c96f7d0398a6c62dff2b2bd6d89
Petros Toupas <ptoupas@gmail.com>
2022-11-28 00:17:52 +0200

    Update SlidingWindow3D (not final)

--- fpgaconvnet/models/modules/SlidingWindow3D.py
+++ fpgaconvnet/models/modules/SlidingWindow3D.py
@@ -28,10 +28,18 @@ class SlidingWindow3D(Module3D):
 
     Attributes
     ----------
-    kernel_size: int
-        kernel size of the convolution layer.
-    stride: int
-        both row and column stride of the convolution layer.
+    kernel_rows: int
+        kernel size of the rows of the convolution layer.
+    kernel_cols: int
+        kernel size of the columns of the convolution layer.
+    kernel_depth: int
+        kernel size of the depth of the convolution layer.
+    stride_rows: int
+        row stride of the convolution layer.
+    stride_cols: int
+        column stride of the convolution layer.
+    stride_depth: int
+        depth stride of the convolution layer.
     pad_top: int
         zero padding for the top of the featuremap.
     pad_right: int
@@ -59,8 +67,14 @@ class SlidingWindow3D(Module3D):
         to `LUT`, `BRAM`, `DSP` and `FF` resources in
         that order.
     """
-    kernel_size: Union[List[int],int]
-    stride: Union[List[int],int]
+    # kernel_size: Union[List[int], int]
+    kernel_rows: int
+    kernel_cols: int
+    kernel_depth: int
+    # stride: Union[List[int],int]
+    stride_rows: int
+    stride_cols: int
+    stride_depth: int
     pad_top: int
     pad_right: int
     pad_front: int
@@ -69,74 +83,58 @@ class SlidingWindow3D(Module3D):
     pad_back: int
 
     def __post_init__(self):
-        pass
-        # format kernel size as a 2 element list
-        if isinstance(self.kernel_size, int):
-            self.kernel_size = [self.kernel_size, self.kernel_size]
-        elif isinstance(self.kernel_size, list):
-            assert len(self.kernel_size) == 2, "Must specify two kernel dimensions"
-        else:
-            raise TypeError
-
-        # format stride as a 2 element list
-        if isinstance(self.stride, int):
-            self.stride = [self.stride, self.stride]
-        elif isinstance(self.stride, list):
-            assert len(self.stride) == 2, "Must specify two stride dimensions"
-        else:
-            raise TypeError
-
         # load the resource model coefficients
+        # TODO: Update resource model coefficients FIXME -> Remove completely
         self.rsc_coef["LUT"] = np.load(
                 os.path.join(os.path.dirname(__file__),
-                "../../coefficients/sliding_window_lut.npy"))
+                "../../coefficients/sliding_window3d_lut.npy"))
         self.rsc_coef["FF"] = np.load(
                 os.path.join(os.path.dirname(__file__),
-                "../../coefficients/sliding_window_ff.npy"))
+                "../../coefficients/sliding_window3d_ff.npy"))
         self.rsc_coef["BRAM"] = np.load(
                 os.path.join(os.path.dirname(__file__),
-                "../../coefficients/sliding_window_bram.npy"))
+                "../../coefficients/sliding_window3d_bram.npy"))
         self.rsc_coef["DSP"] = np.load(
                 os.path.join(os.path.dirname(__file__),
-                "../../coefficients/sliding_window_dsp.npy"))
+                "../../coefficients/sliding_window3d_dsp.npy"))
 
     def utilisation_model(self):
         pass
+        # TODO: Update utilisation model FIXME
         return {
-            "LUT"  : np.array([self.data_width*self.kernel_size[0]*self.kernel_size[1],
-                self.kernel_size[0]*(self.kernel_size[1]-1)*(self.data_width+math.floor(math.log(self.channels,2))),
-                (self.kernel_size[1]-1)*(self.data_width+math.floor(math.log(self.channels*self.cols,2))),
-                (self.kernel_size[0])*(self.kernel_size[1]-1), (self.kernel_size[1]-1)]),
-            "FF"   : np.array([self.data_width*self.kernel_size[0]*self.kernel_size[1],
-                self.kernel_size[0]*(self.kernel_size[1]-1)*(self.data_width+math.floor(math.log(self.channels,2))),
-                (self.kernel_size[1]-1)*(self.data_width+math.floor(math.log(self.channels*self.cols,2))),
-                (self.kernel_size[0])*(self.kernel_size[1]-1), (self.kernel_size[1]-1)]),
+            "LUT"  : np.array([self.data_width*self.kernel_rows*self.kernel_cols,
+                self.kernel_rows*(self.kernel_cols-1)*(self.data_width+math.floor(math.log(self.channels,2))),
+                (self.kernel_cols-1)*(self.data_width+math.floor(math.log(self.channels*self.cols,2))),
+                (self.kernel_rows)*(self.kernel_cols-1), (self.kernel_cols-1)]),
+            "FF"   : np.array([self.data_width*self.kernel_rows*self.kernel_cols,
+                self.kernel_rows*(self.kernel_cols-1)*(self.data_width+math.floor(math.log(self.channels,2))),
+                (self.kernel_cols-1)*(self.data_width+math.floor(math.log(self.channels*self.cols,2))),
+                (self.kernel_rows)*(self.kernel_cols-1), (self.kernel_cols-1)]),
             "DSP"  : np.array([1]),
             "BRAM" : np.array([1]),
         }
 
     def rows_out(self):
-        return int((self.rows_in()-self.kernel_size[0]+self.pad_top+self.pad_bottom)/self.stride[0]+1)
+        return int((self.rows_in()-self.kernel_rows+self.pad_top+self.pad_bottom)/self.stride_rows+1)
 
     def cols_out(self):
-        return int((self.cols_in()-self.kernel_size[1]+self.pad_left+self.pad_right)/self.stride[1]+1)
+        return int((self.cols_in()-self.kernel_cols+self.pad_left+self.pad_right)/self.stride_cols+1)
 
     def depth_out(self):
-        return int((self.cols_in()-self.kernel_size[2]+self.pad_back+self.pad_front)/self.stride[2]+1)
+        return int((self.depth_in()-self.kernel_depth+self.pad_back+self.pad_front)/self.stride_depth+1)
 
     def rate_in(self):
-        pass
-        return (self.rows_in()*self.cols_in())/float(
+        return (self.rows_in()*self.cols_in()*self.depth_in())/float(
                 (self.rows_in()+self.pad_top+self.pad_bottom)*\
-                (self.cols_in()+self.pad_left+self.pad_right))
+                (self.cols_in()+self.pad_left+self.pad_right)*\
+                (self.depth_in()+self.pad_front+self.pad_back))
 
     def rate_out(self):
-        pass
-        return (self.rows_out()*self.cols_out())/float(self.rows*self.cols)
+        return (self.rows_out()*self.cols_out()*self.depth_out())/float(self.rows*self.cols*self.depth)
 
     def pipeline_depth(self):
         pass
-        return (self.cols+self.pad_left+self.pad_right)*(self.channels)*(self.kernel_size[0]-1)+self.channels*self.kernel_size[0]*(self.kernel_size[1]-1)
+        return (self.cols+self.pad_left+self.pad_right)*(self.channels)*(self.kernel_rows-1)+self.channels*self.kernel_rows*(self.kernel_cols-1)
 
     def wait_depth(self):
         pass
@@ -151,16 +149,23 @@ class SlidingWindow3D(Module3D):
         return (self.pad_bottom*self.channels*self.cols+self.pad_left*self.channels+1)
 
     def module_info(self):
-        pass
         # get the base module fields
         info = Module3D.module_info(self)
         # add module-specific info fields
-        info["kernel_size"] = self.kernel_size
-        info["stride"] = self.stride
+        # info["kernel_size"] = self.kernel_size
+        info["kernel_rows"] = self.kernel_rows
+        info["kernel_cols"] = self.kernel_cols
+        info["kernel_depth"] = self.kernel_depth
+        # info["stride"] = self.stride
+        info["stride_rows"] = self.stride_rows
+        info["stride_cols"] = self.stride_cols
+        info["stride_depth"] = self.stride_depth
         info["pad_top"] = self.pad_top
         info["pad_right"] = self.pad_right
+        info["pad_front"] = self.pad_front
         info["pad_bottom"] = self.pad_bottom
         info["pad_left"] = self.pad_left
+        info["pad_back"] = self.pad_back
         # return the info
         return info
 
@@ -181,10 +186,10 @@ class SlidingWindow3D(Module3D):
             coef = self.rsc_coef
         # get the line buffer BRAM estimate
         line_buffer_depth = (self.cols+self.pad_left+self.pad_right)*self.channels+1
-        line_buffer_bram = (self.kernel_size[0]-1) * bram_stream_resource_model(line_buffer_depth, self.data_width)
+        line_buffer_bram = (self.kernel_rows-1) * bram_stream_resource_model(line_buffer_depth, self.data_width)
         # get the window buffer BRAM estimate
         window_buffer_depth = self.channels+1
-        window_buffer_bram = self.kernel_size[0]*(self.kernel_size[1]-1) * bram_stream_resource_model(window_buffer_depth, self.data_width)
+        window_buffer_bram = self.kernel_rows*(self.kernel_cols-1) * bram_stream_resource_model(window_buffer_depth, self.data_width)
         # get the linear model estimation
         rsc = Module3D.rsc(self,coef)
         # add the bram estimation
@@ -193,9 +198,8 @@ class SlidingWindow3D(Module3D):
         return rsc
 
     def visualise(self, name):
-        pass
-        return pydot.Node(name,label="slwin", shape="box",
-                height=self.kernel_size[0],
+        return pydot.Node(name,label="slwin3d", shape="box",
+                height=self.kernel_rows,
                 width=self.cols*self.channels*0.1,
                 style="filled", fillcolor="aquamarine",
                 fontsize=MODULE_3D_FONTSIZE)
@@ -243,16 +247,16 @@ class SlidingWindow3D(Module3D):
             self.cols_out(),
             self.depth_out(),
             self.channels,
-            self.kernel_size[0],
-            self.kernel_size[1],
-            self.kernel_size[2]),dtype=float)
+            self.kernel_rows,
+            self.kernel_cols,
+            self.kernel_depth),dtype=float)
 
         for index,_ in np.ndenumerate(out):
             out[index] = data_padded[
                 index[0],
-                index[1]*self.stride[0]+index[5],
-                index[2]*self.stride[1]+index[6],
-                index[3]*self.stride[2]+index[7],
+                index[1]*self.stride_rows+index[5],
+                index[2]*self.stride_cols+index[6],
+                index[3]*self.stride_depth+index[7],
                 index[4]]
 
         return out
