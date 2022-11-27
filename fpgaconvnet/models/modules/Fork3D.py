76a9905f809e2f1dd8a7c4d404680d63b3dfcff7
Petros Toupas <ptoupas@gmail.com>
2022-11-27 21:48:49 +0200

    Update Fork3D module

--- fpgaconvnet/models/modules/Fork3D.py
+++ fpgaconvnet/models/modules/Fork3D.py
@@ -21,55 +21,51 @@ from fpgaconvnet.models.modules import Module3D, MODULE_3D_FONTSIZE
 
 @dataclass
 class Fork3D(Module3D):
-    kernel_size: Union[List[int],int]
+    # kernel_size: Union[List[int], int]
+    kernel_rows: int
+    kernel_cols: int
+    kernel_depth: int
     coarse: int
 
     def __post_init__(self):
-        pass
-        # format kernel size as a 2 element list
-        if isinstance(self.kernel_size, int):
-            self.kernel_size = [self.kernel_size, self.kernel_size]
-        elif isinstance(self.kernel_size, list):
-            assert len(self.kernel_size) == 2, "Must specify two kernel dimensions"
-        else:
-            raise TypeError
-
         # load the resource model coefficients
+        # TODO: Update resource model coefficients FIXME
         self.rsc_coef["LUT"] = np.load(
                 os.path.join(os.path.dirname(__file__),
-                "../../coefficients/fork_lut.npy"))
+                "../../coefficients/fork3d_lut.npy"))
         self.rsc_coef["FF"] = np.load(
                 os.path.join(os.path.dirname(__file__),
-                "../../coefficients/fork_ff.npy"))
+                "../../coefficients/fork3d_ff.npy"))
         self.rsc_coef["BRAM"] = np.load(
                 os.path.join(os.path.dirname(__file__),
-                "../../coefficients/fork_bram.npy"))
+                "../../coefficients/fork3d_bram.npy"))
         self.rsc_coef["DSP"] = np.load(
                 os.path.join(os.path.dirname(__file__),
-                "../../coefficients/fork_dsp.npy"))
+                "../../coefficients/fork3d_dsp.npy"))
 
     def utilisation_model(self):
-        pass
+        # TODO: Update utilisation model FIXME
         return {
-            "LUT"  : np.array([math.ceil(math.log(self.channels*self.rows*self.cols,2))]),
-            "FF"   : np.array([math.ceil(math.log(self.channels*self.rows*self.cols,2))]),
+            "LUT"  : np.array([math.ceil(math.log(self.channels*self.rows*self.cols*self.depth,2))]),
+            "FF"   : np.array([math.ceil(math.log(self.channels*self.rows*self.cols*self.depth,2))]),
             "DSP"  : np.array([1]),
             "BRAM" : np.array([1]),
         }
 
     def module_info(self):
-        pass
         # get the base module fields
         info = Module3D.module_info(self)
         # add module-specific info fields
         info["coarse"] = self.coarse
-        info["kernel_size"] = self.kernel_size
+        # info["kernel_size"] = self.kernel_size
+        info["kernel_rows"] = self.kernel_rows
+        info["kernel_cols"] = self.kernel_cols
+        info["kernel_depth"] = self.kernel_depth
         # return the info
         return info
 
     def visualise(self, name):
-        pass
-        return pydot.Node(name,label="fork", shape="box",
+        return pydot.Node(name,label="fork3d", shape="box",
                 style="filled", fillcolor="azure",
                 fontsize=MODULE_3D_FONTSIZE)
 
@@ -79,9 +75,9 @@ class Fork3D(Module3D):
         assert data.shape[1] == self.cols    , "ERROR: invalid column dimension"
         assert data.shape[2] == self.depth    , "ERROR: invalid depth dimension"
         assert data.shape[3] == self.channels, "ERROR: invalid channel dimension"
-        assert data.shape[4] == self.kernel_size[0]  , "ERROR: invalid kernel row dimension"
-        assert data.shape[5] == self.kernel_size[1]  , "ERROR: invalid kernel column dimension"
-        assert data.shape[6] == self.kernel_size[2]  , "ERROR: invalid kernel depth dimension"
+        assert data.shape[4] == self.kernel_rows  , "ERROR: invalid kernel row dimension"
+        assert data.shape[5] == self.kernel_cols  , "ERROR: invalid kernel column dimension"
+        assert data.shape[6] == self.kernel_depth  , "ERROR: invalid kernel depth dimension"
 
         out = np.ndarray((
             self.rows,
@@ -89,9 +85,9 @@ class Fork3D(Module3D):
             self.depth,
             self.channels,
             self.coarse,
-            self.kernel_size[0],
-            self.kernel_size[1],
-            self.kernel_size[2]),dtype=float)
+            self.kernel_rows,
+            self.kernel_cols,
+            self.kernel_depth),dtype=float)
 
         for index,_ in np.ndenumerate(out):
             out[index] = data[
