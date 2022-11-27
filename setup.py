74455f4868e2027d4ed796c32f80d81c601d0242
Petros Toupas <ptoupas@gmail.com>
2022-11-28 01:11:41 +0200

    Add toml dependency to setup.py

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setuptools.setup(
         "onnxoptimizer==0.3.0",
         "ddt>=1.4.2",
         "sklearn",
+        "toml",
         "matplotlib",
         "coverage==5.5",
         "onnxsim>=0.4.8",
