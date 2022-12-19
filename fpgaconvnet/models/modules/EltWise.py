c7d69ec9f7dc9c913b3586687b2d27dfb90ac0c8
Petros Toupas <ptoupas@gmail.com>
2022-12-19 19:05:57 +0200

    fix eltwise rsc model

--- fpgaconvnet/models/modules/EltWise.py
+++ fpgaconvnet/models/modules/EltWise.py
@@ -33,11 +33,14 @@ class EltWise(Module):
             estimated resource usage of the module. Uses the
             resource coefficients for the estimate.
         """
+        # get the channel buffer BRAM estimate
+        channel_buffer_bram = bram_memory_resource_model(int(self.channels), self.data_width)
+
         return {
             "LUT"   : 49,
             "FF"    : 23,
-            "BRAM"  : 0,
-            "DSP"   : 0
+            "BRAM"  : channel_buffer_bram if self.broadcast else 0,
+            "DSP"   : 0 if self.eltwise_type == "add" else 1
         }
 
     def module_info(self):
@@ -75,26 +78,6 @@ class EltWise(Module):
 
         else:
             raise ValueError(f"{self.backend} backend not supported")
-
-    def rsc(self,coef=None, model=None):
-        # use module resource coefficients if none are given
-        if coef == None:
-            coef = self.rsc_coef
-
-        # get the channel buffer BRAM estimate
-        channel_buffer_bram = bram_memory_resource_model(int(self.channels), self.data_width)
-
-        # get the linear model estimation
-        rsc = Module.rsc(self, coef, model)
-
-        # add the bram estimation
-        rsc["BRAM"] = channel_buffer_bram if self.broadcast else 0
-
-        # ensure zero DSPs
-        rsc["DSP"] = 0 if self.eltwise_type == "add" else 1
-
-        # return the resource usage
-        return rsc
 
     def functional_model(self, data):
         # check input dimensionality
