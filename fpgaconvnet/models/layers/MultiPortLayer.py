36d1544343b7c26dad89025c62fa258b4a781182
Petros Toupas <ptoupas@gmail.com>
2023-12-28 19:04:55 +0200

    Update MultiPortLayer.py

--- fpgaconvnet/models/layers/MultiPortLayer.py
+++ fpgaconvnet/models/layers/MultiPortLayer.py
@@ -395,8 +395,7 @@ class MultiPortLayer:
 
     def latency(self):
         # return max(self.latency_in(), self.latency_out())
-        latencies = [ self.modules[module].latency() for module in self.modules ]
-        return max(latencies)
+        return max(module.latency() for module in self.modules.values())
 
     def start_depth(self):
         return 2 # number of input samples required to create a complete output channel
